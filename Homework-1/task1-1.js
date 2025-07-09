7f72d42178275a4bced0ed2d51272c31fe46e192
DanaXans <danaxans@gmail.com>
2025-07-09 21:00:45 +0300

    commit

---
+++ Homework-1/task1-1.js
@@ -0,0 +1,46 @@
+/*#dYQNrBV*/
+let text1 = "hello";
+console.log(text1);
+let text2 = "owu";
+console.log(text2);
+let text3 = "com";
+console.log(text3);
+let text4 = "ua";
+console.log(text4);
+
+let num = 1;
+console.log(num);
+let num1 = 10;
+console.log(num1);
+let num2 = -999;
+console.log(num2);
+let num3 = 123;
+console.log(num3);
+let n1 = 3.14;
+console.log(n1);
+let n2 = 2.7;
+console.log(n2);
+let n3 = 16;
+console.log(n3);
+
+let TR = true;
+console.log(TR);
+let FL = false;
+console.log(FL);
+
+/*#6Qb97gsv*/
+let firstName = "Богдана";
+let middleName = "Валеріївна";
+let lastName = "Шаповалова";
+let person = firstName + ' ' + middleName  + ' ' + lastName;
+console.log(person);
+
+/*#4N0y5tufA*/
+let a = 100;
+console.log(typeof a );
+let b = 100;
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+
