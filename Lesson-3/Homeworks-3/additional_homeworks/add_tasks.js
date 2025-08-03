c40fb0c4aa05028f306ff7057eda009853b670af
DanaXans <danaxans@gmail.com>
2025-08-03 19:39:04 +0300

    change add_tasks.js

--- Lesson-3/Homeworks-3/additional_homeworks/add_tasks.js
+++ Lesson-3/Homeworks-3/additional_homeworks/add_tasks.js
@@ -21,10 +21,44 @@ console.log(array[2]);
 
 
 /*qLQLJSeN7i*/
-let arrayOfItems = [1, 'string', false, 3.14, 'task'];
+let arrayOfItems = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+let i = 0;
 /*1. перебрати його циклом while*/
-
+/*while (i < arrayOfItems.length) {
+    console.log(arrayOfItems[i]);
+    i++;
+}*/
 /*2. перебрати його циклом for*/
-for (let i = array; i<5; i++) {
-console.log(arrayOfItems);
+for (let i = 0; i < arrayOfItems.length; i++) {
+    console.log(arrayOfItems[i]);
+}
+/*3. перебрати циклом while та вивести  числа тільки з непарним індексом*/
+let k = 1;
+while (k < arrayOfItems.length) {
+    console.log(arrayOfItems[k]);
+    k += 2;
+}
+/*4. перебрати циклом for та вивести  числа тільки з непарним індексом*/
+for (let i = 1; i < arrayOfItems.length; i += 2) {
+    console.log(arrayOfItems[i]);
+}
+/*5. перебрати циклом while та вивести  числа тільки парні  значення*/
+/*while (i < arrayOfItems.length) {
+    if (i % 2 === 0) {
+        console.log(arrayOfItems[i])
+    }
+    i++;
+}*/
+/*6. перебрати циклом for та вивести  числа тільки парні  значення*/
+for (let i = 0; i < arrayOfItems.length; i++) {
+    if (i % 2 === 0) {
+        console.log(arrayOfItems[i])
+    }
+    /*7. замінити кожне число, кратне 3, на слово “okten”*/
+    while (i<arrayOfItems.length) {
+        if (i % 3 === 0) {
+            console.log(arrayOfItems[i])
+        }
+        i++;
+    }
 }
