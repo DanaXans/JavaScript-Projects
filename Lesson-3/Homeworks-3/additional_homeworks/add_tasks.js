2a671f74169b81f45a14a8eef13f7ec59b5f31bf
DanaXans <danaxans@gmail.com>
2025-08-03 13:42:16 +0300

    add add_tasks.html, add_tasks.js

---
+++ Lesson-3/Homeworks-3/additional_homeworks/add_tasks.js
@@ -0,0 +1,30 @@
+/*WpkK0ZH1*/
+/*–створити масив з:*/
+/*– з 5 числових значень*/
+let arr = [1, 8, 123, 3.14];
+console.log(arr);
+/*– з 5 стічкових значень*/
+let arr2 = ['time', 'pipe', 'word', 'life', 'game'];
+console.log(arr2);
+/*– з 5 значень стрічкового, числового та булевого типу*/
+let arr3 = ['divine', 13, true];
+console.log(arr3);
+/*– та вивести його в консоль*/
+
+
+/*4aDbSgh*/
+/*— Створити пустий масив. Наповнити його будь-якими значеннями, звертаючись до конкретного індексу. Вивести в консоль*/
+let array = ['console', 14, true]
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+
+
+/*qLQLJSeN7i*/
+let arrayOfItems = [1, 'string', false, 3.14, 'task'];
+/*1. перебрати його циклом while*/
+
+/*2. перебрати його циклом for*/
+for (let i = array; i<5; i++) {
+console.log(arrayOfItems);
+}
